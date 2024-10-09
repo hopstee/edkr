533f72971f0edf58f10bb39fcdb058582cb284b8
Edik <edik.krivovyaschuk@crossnetics.io>
2024-10-09 21:04:05 +0300

    Not found page localization

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,26 +1,13 @@
-'use client'
+'use client';
 
-import { useRouter } from '@/i18n/routing';
-import { ArrowLeft } from 'lucide-react';
+import Error from 'next/error';
 
 export default function NotFound() {
-    const router = useRouter()
-
-    const handleGoBack = () => {
-        router.back()
-    }
-
     return (
-        <div className='h-screen w-full flex flex-col items-center justify-center'>
-            <h1 className='text-2xl text-neutral-600 dark:text-neutral-300'>
-                Nothing here 🤷‍♂️
-            </h1>
-            <div className='flex items-center cursor-pointer rounded-xl hover:underline mt-5' onClick={handleGoBack}>
-                <ArrowLeft className='size-4 mr-2' />
-                <h6>
-                    Go back
-                </h6>
-            </div>
-        </div>
-    )
+        <html lang="en">
+            <body>
+                <Error statusCode={404} />
+            </body>
+        </html>
+    );
 }
